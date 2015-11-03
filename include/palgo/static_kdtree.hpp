4509c6b301260279931d7f8236e2e23680514590
Mark Grimes <mark.grimes@cern.ch>
2015-11-03 12:13:05 +0000

    Change iterator to work on subtree range rather than single index

--- include/palgo/static_kdtree.hpp
+++ include/palgo/static_kdtree.hpp
@@ -18,6 +18,17 @@
 //
 namespace
 {
+	/** @brief Function to return the number of elements in a container.
+	 *
+	 * Required because cl::sycl::accessor doesn't have a "size" method; it has a "get_count"
+	 * method instead ("get_size" is get_count*sizeof(T)). This is the default for normal STL
+	 * containers, the SYCL accessor special case will be in a specialisation. */
+	template<class T>
+	inline size_t dataSize( const T& container )
+	{
+		return container.size();
+	}
+
 	/** @brief Info struct to get the type of the containers elements.
 	 *
 	 * Required because cl::sycl::accessor doesn't have a value_type. This is the default for
@@ -30,6 +41,12 @@ namespace
 
 // If ComputeCpp is being used, add template specialisations
 #ifdef RUNTIME_INCLUDE_SYCL_ACCESSOR_H_ // This is the include guard for SYCL/accessor.h
+	template<class T_dataType,int dimensions,cl::sycl::access::mode accessMode,cl::sycl::access::target target>
+	inline size_t dataSize( const cl::sycl::accessor<T_dataType,dimensions,accessMode,target>& container )
+	{
+		return container.get_count();
+	}
+
 	/** @brief Template specialisation to retrieve the type of a SYCL accessor's elements. */
 	template<class T_dataType,int dimensions,cl::sycl::access::mode accessMode,cl::sycl::access::target target>
 	struct ContainerTypeInfo<cl::sycl::accessor<T_dataType,dimensions,accessMode,target> >
@@ -196,32 +213,58 @@ namespace palgo
 		public:
 			typedef typename ContainerTypeInfo<T_datastore>::value_type value_type;
 		public:
-			Iterator( size_t index, const T_datastore& store ) : index_(index), pStore_(&store) {}
-			bool operator==( const Iterator& otherIterator ) { return index_==otherIterator.index_; }
+			Iterator( size_t leftLimit, size_t rightLimit, const T_datastore& store ) : subTreeFarLeft_(leftLimit), subTreeFarRight_(rightLimit), pStore_(&store) {}
+			bool operator==( const Iterator& otherIterator ) { return subTreeFarLeft_==otherIterator.subTreeFarLeft_ && subTreeFarRight_==otherIterator.subTreeFarRight_; }
 			bool operator!=( const Iterator& otherIterator ) { return !(*this==otherIterator); }
-			value_type operator*() { return (*pStore_)[index_]; }
-			const value_type* operator->() const { return &((*pStore_)[index_]); }
+			value_type operator*() { return (*pStore_)[currentIndex()]; }
+			const value_type* operator->() const { return &((*pStore_)[currentIndex()]); }
+			Iterator& goto_parent()
+			{
+				// Pretty poor algorithm, but I just want to get something working. I'll look at
+				// optimisations later. Start with a root Iterator and traverse down until the step
+				// before the current state, then copy that position.
+				// TODO - optimise with a decent algorithm
+				size_t current=currentIndex();
+				Iterator stepBefore( 0, pStore_->size(), *pStore_ ); // start with an iterator to the root node
+				while( stepBefore.currentIndex()!=current )
+				{
+					subTreeFarLeft_=stepBefore.subTreeFarLeft_;
+					subTreeFarRight_=stepBefore.subTreeFarRight_;
+					if( stepBefore.currentIndex()<current ) stepBefore.goto_right_child();
+					else stepBefore.goto_left_child();
+				}
+				return *this;
+			}
+			Iterator& goto_left_child() { subTreeFarRight_=currentIndex(); return *this; }
+			Iterator& goto_right_child() { subTreeFarLeft_=currentIndex()+1; return *this; }
+			bool has_parent() const { if( subTreeFarLeft_==0 && subTreeFarRight_==pStore_->size() ) return false; else return true; }
+			bool has_left_child() const { return currentIndex()!=subTreeFarLeft_; }
+			bool has_right_child() const { return currentIndex()+1!=subTreeFarRight_; }
 		private:
-			size_t index_;
+			size_t subTreeFarLeft_,subTreeFarRight_;
+			inline size_t currentIndex() const { return (subTreeFarLeft_+subTreeFarRight_)/2; }
 			const T_datastore* pStore_;
 		};
 	public:
 		typedef typename ContainerTypeInfo<T_datastore>::value_type value_type; // Can't use T_datastore::value_type because sycl::accessor doesn't have it
 		typedef Iterator const_iterator;
 	public:
-		static_kdtree( T_datastore data, bool presorted=false ) : data_(data)
+		static_kdtree( T_datastore data, bool presorted=false ) : data_(data), size_( ::dataSize(data_) )
 		{
 			if( presorted ) return; // If the data is already in the correct order, no need to do anything else
 
 			// SYCL doesn't allow exceptions, but while I'm testing in host code I'll put this in for now.
 			throw std::logic_error( "Construction of static_kdtrees where the input is not already sorted has not been implemented yet" );
 		}
+		Iterator root() const { return Iterator( 0, size_, data_ ); }//{ return Iterator( 0, ::dataSize(data_), data_ ); }
+		Iterator end() const { return Iterator( size_, size_, data_ ); }
 		Iterator nearest_neighbour( const value_type& query ) const
 		{
-			return Iterator(0,data_);
+			return end();
 		}
 	protected:
 		T_datastore data_;
+		size_t size_; // Need to store this because ComputeCpp currently segfaults if accessor size is queried in the kernel. ToDo - remove this once ComputeCpp is fixed.
 	};
 }
 
